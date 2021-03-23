a05711a9665d85d92ba0953dc788b85b122c7de8
Catalano Nicolas <nec.catalano@gmail.com>
2021-03-22 22:03:19 -0300

    Subo el proyecto a github, siguiendo hasta la relacion entre modelos

---
+++ __manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+         Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "Catalano Nicolas",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    'category': 'test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/openacademy.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ models/models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = 'Guardamos todos los cursos disponibles'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text(string="Description")
+
+    responsible_id = fields.Many2one('res.users',
+                   ondelete="set null", string="Responsible", index=True)
+    session_id = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in day")
+    seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course', ondelete="cascade",
+                                string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="attendee")
+
+
